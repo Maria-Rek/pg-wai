f175ebe58f2b868abb320fd0af808354d760abf1
Marysia <maria.rek04@wp.pl>
2025-01-05 20:38:18 +0100

    przed connect

--- src/web/front_controller.php
+++ src/web/front_controller.php
@@ -1,20 +1,15 @@
 <?php
 require '../../vendor/autoload.php';
-
-
 require_once '../dispatcher.php';
 require_once '../routing.php';
 require_once '../controllers.php';
 
 session_start();
 
-
-//CZYSZCZENIE BAZY
 function clear_db(){
     $db = get_db();
     $db->pictures->drop();
 }
-
 //clear_db();
 
 $action_url = $_GET['action'];
